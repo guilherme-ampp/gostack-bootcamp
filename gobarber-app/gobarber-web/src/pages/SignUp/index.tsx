300d0c5e06702ecf4f7df44d2bd47422a1678322
Guilherme de Almeida <guilherme.ampp@gmail.com>
2020-04-30 22:16:59 -0300

    Missed modifications after changing folder to hooks/
    * Adjusted SignIn and SignUp index files.

--- gobarber-app/gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-app/gobarber-web/src/pages/SignUp/index.tsx
@@ -32,8 +32,12 @@ const SignUp: React.FC = () => {
                 abortEarly: false,
             });
         } catch (err) {
-            const errors = getValidationErrors(err);
-            formRef.current?.setErrors(errors);
+            if (err instanceof Yup.ValidationError) {
+                const errors = getValidationErrors(err);
+                formRef.current?.setErrors(errors);
+            }
+
+            // trigger a toast message
         }
     }, []);
 
