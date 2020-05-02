f64ef1a7d763de3045e995774245f8ec429c9b6d
Guilherme de Almeida <guilherme.ampp@gmail.com>
2020-05-02 15:06:16 -0300

    Added routes and navigation between Sign In and Sign Up
    * Addressing transition animations

--- gobarber-app/gobarbermobile/src/pages/SignUp/index.tsx
+++ gobarber-app/gobarbermobile/src/pages/SignUp/index.tsx
@@ -1,8 +1,66 @@
 import React from 'react';
-import { Container } from './styles';
+import {
+    Image,
+    View,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logo.png';
+import {
+    Container,
+    Title,
+    BackToSignInButtonText,
+    BackToSignInButton,
+} from './styles';
 
 const SignUp: React.FC = () => {
-    return <Container />;
+    const navigation = useNavigation();
+
+    return (
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+            enabled
+        >
+            <ScrollView
+                keyboardShouldPersistTaps="handled"
+                contentContainerStyle={{ flex: 1 }}
+            >
+                <Container>
+                    <Image source={logoImg} />
+
+                    <View>
+                        <Title>Sign Up</Title>
+                    </View>
+                    <Input name="name" icon="user" placeholder="Name" />
+                    <Input name="email" icon="mail" placeholder="Email" />
+                    <Input name="password" icon="lock" placeholder="Password" />
+                    <Button
+                        onPress={() => {
+                            console.log('');
+                        }}
+                    >
+                        Create account
+                    </Button>
+                </Container>
+            </ScrollView>
+
+            <BackToSignInButton
+                onPress={() => {
+                    navigation.goBack();
+                }}
+            >
+                <Icon name="arrow-left" size={20} color="#fff" />
+                <BackToSignInButtonText>Back to sign in</BackToSignInButtonText>
+            </BackToSignInButton>
+        </KeyboardAvoidingView>
+    );
 };
 
 export default SignUp;
