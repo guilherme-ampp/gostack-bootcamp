c59249b2e92708d7629e61ae20891fe9e0d58f82
Guilherme de Almeida <guilherme.ampp@gmail.com>
2020-05-02 14:50:51 -0300

    Finished Sign In screen UI and styles
    * Addressing different UI behavior on Android and iOS when the virtual keyboard comes up

--- gobarber-app/gobarbermobile/src/pages/SignIn/index.tsx
+++ gobarber-app/gobarbermobile/src/pages/SignIn/index.tsx
@@ -1,27 +1,67 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+    Image,
+    View,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
-import { Container, Title } from './styles';
+import {
+    Container,
+    Title,
+    ForgotPassword,
+    ForgotPasswordText,
+    CreateAccountButtonText,
+    CreateAccountButton,
+} from './styles';
 
 const SignIn: React.FC = () => {
     return (
-        <Container>
-            <Image source={logoImg} />
-
-            <Title>Login</Title>
-            <Input name="email" icon="mail" placeholder="Email" />
-            <Input name="password" icon="lock" placeholder="Password" />
-            <Button
-                onPress={() => {
-                    console.log('');
-                }}
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+            enabled
+        >
+            <ScrollView
+                keyboardShouldPersistTaps="handled"
+                contentContainerStyle={{ flex: 1 }}
             >
-                Sign in
-            </Button>
-        </Container>
+                <Container>
+                    <Image source={logoImg} />
+
+                    <View>
+                        <Title>Login</Title>
+                    </View>
+                    <Input name="email" icon="mail" placeholder="Email" />
+                    <Input name="password" icon="lock" placeholder="Password" />
+                    <Button
+                        onPress={() => {
+                            console.log('');
+                        }}
+                    >
+                        Sign in
+                    </Button>
+
+                    <ForgotPassword onPress={() => {}}>
+                        <ForgotPasswordText>
+                            Forgot password?
+                        </ForgotPasswordText>
+                    </ForgotPassword>
+                </Container>
+            </ScrollView>
+
+            <CreateAccountButton onPress={() => {}}>
+                <Icon name="log-in" size={20} color="#ff9000" />
+                <CreateAccountButtonText>
+                    Create account
+                </CreateAccountButtonText>
+            </CreateAccountButton>
+        </KeyboardAvoidingView>
     );
 };
 
